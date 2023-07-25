e3c6e60c88f00a194dcde03c90d610a214b39290
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2023-07-24 22:17:57 -0400

    Genera data source

---
+++ build_genera_dataSource.py
@@ -0,0 +1,158 @@
+import json
+import datetime
+import os
+import polars as pl
+import shutil
+import requests
+
+# If directory 'genera' doesn't exist, create it
+if not os.path.exists('genera'):
+    os.makedirs('genera')
+if not os.path.exists('genera/historical'):
+    os.makedirs('genera/historical')
+
+# Format as "07/19/2023 12:55 PM"
+timestamp_saved = datetime.datetime.now().strftime("%m/%d/%Y %I:%M %p")
+print(timestamp_saved)
+
+# Get the data from the API
+url = 'https://aeepr.com/es-pr/generacion/Documents/DataJS/dataSource.js'
+r = requests.get(url)
+data_source_script = r.text
+# regions_dict['timestamp_saved'] = timestamp_saved
+
+fname = 'genera/dataSource.js'
+with open(fname, 'w') as f:
+    f.write(f"const timestamp_saved = '{timestamp_saved}';\n")
+    f.write(data_source_script)
+
+fname_run = 'genera/dataSource_run.js'
+shutil.copyfile(fname, fname_run)
+with open(fname_run, 'a') as f:
+    f.write(
+        """
+
+const fs = require('fs');
+
+const metadata = {timestamp_saved, dataFechaActualizado: dataFechaAcualizado};
+
+fs.writeFileSync('genera/metadata.json', JSON.stringify(metadata, null, 2));
+fs.writeFileSync('genera/dataFuelCost.json', JSON.stringify(dataFuelCost, null, 2));
+fs.writeFileSync('genera/dataByFuel.json', JSON.stringify(dataByFuel, null, 2));
+fs.writeFileSync('genera/dataMetrics.json', JSON.stringify(dataMetrics, null, 2));
+
+fs.writeFileSync('genera/dataLoadPerSite.json',
+JSON.stringify(dataLoadPerSite, null, 2));
+        """
+
+    )
+
+# Run the script
+os.system(f'node {fname_run}')
+# delete the run file
+os.remove(fname_run)
+
+# Start reopening files
+with open('genera/metadata.json') as f:
+    metadata = json.load(f)
+timestamp_saved = metadata['timestamp_saved']
+dataFechaActualizado = metadata['dataFechaActualizado']
+
+print(metadata)
+
+for fbase in ['dataFuelCost', 'dataByFuel', 'dataMetrics']:
+    fname = f'genera/{fbase}.json'
+    with open(fname) as f:
+        data = json.load(f)
+
+    df = pl.DataFrame(data)
+    initial_cols = df.columns
+    df = df.select([
+        pl.lit(timestamp_saved).alias('timestamp_saved'),
+        pl.lit(dataFechaActualizado).alias('dataFechaActualizado'),
+        *initial_cols
+    ])
+
+    # To match historical data
+    cast_map = {
+        'Index': int,
+    }
+    for col in cast_map:
+        if col in df.columns:
+            df = df.with_columns([pl.col(col).cast(cast_map[col])])
+
+
+
+    print(fbase)
+    with pl.Config(tbl_cols=-1):
+        print(df)
+    print()
+
+    out_fname = f'genera/historical/{fbase}_historical.csv'
+    if os.path.exists(out_fname):
+        existing_df = pl.read_csv(out_fname)
+        
+        df = existing_df.vstack(df)
+
+    df.write_csv(out_fname)
+    print(f'Wrote {out_fname}')
+    print()
+
+
+# Esta es diferente
+with open('genera/dataLoadPerSite.json') as f:
+    dataLoadPerSite = json.load(f)
+
+print('dataLoadPerSite')
+# print(json.dumps(dataLoadPerSite, indent=2, sort_keys=False, ensure_ascii=False))
+# print()
+
+flat_dataLoadPerSite = []
+for site in dataLoadPerSite:
+    site['site_index'] = site['Index']
+    del site['Index']
+
+    for unit in site['units']:
+        unit['unit_index'] = unit['Index']
+        del unit['Index']
+
+        row = {
+            **site,
+            **unit,
+        }
+        del row['units']
+        flat_dataLoadPerSite.append(row)
+
+df = pl.DataFrame(flat_dataLoadPerSite)
+initial_cols = df.columns
+df = df.select([
+    pl.lit(timestamp_saved).alias('timestamp_saved'),
+    pl.lit(dataFechaActualizado).alias('dataFechaActualizado'),
+    
+    # Not casting all of it because some are strings in the original data (idk why)
+    pl.col('site_index'),#.cast(int),
+    'Type', 'Desc',
+    pl.col('SiteTotal').cast(int),
+    pl.col('unit_index'),#.cast(int),
+    'Unit',
+    pl.col('MW').cast(int),
+    'MVar',
+    pl.col('Cost').cast(float), 
+    'ParentId',
+])
+with pl.Config(tbl_cols=-1):
+    print(df)
+
+missing_cols = set(initial_cols) - set(df.columns)
+if len(missing_cols) > 0:
+    raise ValueError(f'Missing columns: {missing_cols}')
+
+out_fname = 'genera/historical/dataLoadPerSite_historical.csv'
+if os.path.exists(out_fname):
+    existing_df = pl.read_csv(out_fname)
+    
+    df = existing_df.vstack(df)
+
+df.write_csv(out_fname)
+print(f'Wrote {out_fname}')
+print()
--- build_outageTowns.py
+++ build_outageTowns.py
@@ -1,7 +1,5 @@
 import json
 import datetime
-import os
-import polars as pl
 import requests
 
 # Format as "07/19/2023 12:55 PM"
@@ -137,52 +135,3 @@ with open(fname, 'w', encoding='UTF-8') as f:
     json.dump(towns_dict, f, indent=2, sort_keys=True, ensure_ascii=False)
 
 print(json.dumps(towns_dict, indent=2, sort_keys=True, ensure_ascii=False))
-
-# "Reopen" the file
-
-# timestamp = regions_dict['timestamp']
-# regions_list = regions_dict['regions']
-# totals_obj = regions_dict['totals']
-
-# new_list = []
-
-# totals_obj['type'] = 'totals'
-# totals_obj['name'] = None
-# totals_obj['percentageClientsWithoutService'] = totals_obj['totalPercentageWithoutService']
-# totals_obj['percentageClientsWithService'] = totals_obj['totalPercentageWithService']
-# del totals_obj['totalPercentageWithoutService']
-# del totals_obj['totalPercentageWithService']
-# new_list.append(totals_obj)
-
-# for region in regions_list:
-#     region['type'] = 'region'
-#     new_list.append(region)
-
-# for row in new_list:
-#     row['timestamp'] = timestamp
-#     row['timestamp_saved'] = timestamp_saved
-
-# print(json.dumps(new_list, indent=2, sort_keys=True))
-
-# df = (
-#     pl.DataFrame(new_list)
-#     .select([
-#         'timestamp_saved',
-#         'timestamp', 'type', pl.col('name').alias('region_name'),
-#         'totalClients', 'totalClientsWithoutService', 'totalClientsWithService',
-#         'percentageClientsWithoutService', 'percentageClientsWithService',
-
-#     ])
-# )
-# with pl.Config(tbl_cols=-1):
-#     print(df)
-
-# out_fname = 'regionsWithoutService_historical.csv'
-# if os.path.exists(out_fname):
-#     existing_df = pl.read_csv(out_fname)
-#     if ('csv_timestamp' in existing_df.columns):
-#         existing_df = existing_df.rename({'csv_timestamp': 'timestamp_saved'})
-#     df = existing_df.vstack(df)
-
-# df.write_csv(out_fname)
-# print(f'Wrote {out_fname}')
