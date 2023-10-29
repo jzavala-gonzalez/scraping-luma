9f2237063bcc5b21b94ad09def03c02d4a957a74
Automated <actions@users.noreply.github.com>
2023-10-29 06:35:50 +0000

    Update genera/dataSource Sun Oct 29 06:35:50 UTC 2023

--- genera/dataSource.js
+++ genera/dataSource.js
@@ -1,26 +1,4 @@
-const timestamp_saved = '10/29/2023 06:30 AM';
-const dataFechaAcualizado = '10/29/2023' + ' 2:21:18 AM';
+const timestamp_saved = '10/29/2023 06:35 AM';
+const dataFechaAcualizado = '10/29/2023' + ' 2:31:18 AM';
 const dataFuelCost = [
 	{place: 'San Juan', value: 98},
-	{place: 'San Juan CC', value: 127},
-	{place: 'Palo Seco', value: 97},
-	{place: 'Aguirre', value: 96},
-	{place: 'Aguirre CC', value: 127},
-	{place: 'Costa Sur', value: 92},
-	{place: 'Cambalache', value: 134},
-	{place: 'Mayaguez', value: 127},
-	{place: 'Costa Sur LNG', value: 9},
-];
-const dataByFuel = [
-	{fuel: 'Bunker', value: 27},
-	{fuel: 'Diesel', value: 15},
-	{fuel: 'LNG', value: 45},
-	{fuel: 'Coal', value: 12},
-	{fuel: 'Renew', value: 0},
-];
-const dataMetrics = [
-	{Index:'0', Desc:'Total de Generación', value: 2069},
-	{Index:'1', Desc:'PREPA', value: 57},
-	{Index:'2', Desc:'COGEN', value: 43},
-	{Index:'3', Desc:'Reserva en Rotación', value: 568},
-	{Index:'4', Desc:'Reserva Operacional', value: 1170},
