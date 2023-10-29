53db1457ab649df6e8f26fdd4727ff2bc7606207
Automated <actions@users.noreply.github.com>
2023-10-29 07:10:53 +0000

    Update genera/dataSource Sun Oct 29 07:10:53 UTC 2023

--- genera/dataSource.js
+++ genera/dataSource.js
@@ -1,3 +1,3 @@
-const timestamp_saved = '10/29/2023 07:05 AM';
-const dataFechaAcualizado = '10/29/2023' + ' 3:01:18 AM';
+const timestamp_saved = '10/29/2023 07:10 AM';
+const dataFechaAcualizado = '10/29/2023' + ' 3:06:18 AM';
 const dataFuelCost = [
