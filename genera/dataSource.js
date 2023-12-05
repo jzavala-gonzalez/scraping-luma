e939244224dc6e61d840240f7dfaf6de8c84a1aa
Automated <actions@users.noreply.github.com>
2023-12-05 12:00:56 +0000

    Update genera/dataSource Tue Dec  5 12:00:56 UTC 2023

--- genera/dataSource.js
+++ genera/dataSource.js
@@ -1,1 +1,96 @@
-const timestamp_saved = '12/05/2023 11:55 AM';
+const timestamp_saved = '12/05/2023 12:00 PM';
+const dataFechaAcualizado = '12/5/2023' + ' 7:51:24 AM';
+const dataFuelCost = [
+	{place: 'San Juan', value: 98},
+	{place: 'San Juan CC', value: 127},
+	{place: 'Palo Seco', value: 97},
+	{place: 'Aguirre', value: 96},
+	{place: 'Aguirre CC', value: 127},
+	{place: 'Costa Sur', value: 92},
+	{place: 'Cambalache', value: 134},
+	{place: 'Mayaguez', value: 127},
+	{place: 'Costa Sur LNG', value: 9},
+];
+const dataByFuel = [
+	{fuel: 'Bunker', value: 22},
+	{fuel: 'Diesel', value: 0},
+	{fuel: 'LNG', value: 65},
+	{fuel: 'Coal', value: 12},
+	{fuel: 'Renew', value: 1},
+];
+const dataMetrics = [
+	{Index:'0', Desc:'Total de Generación', value: 2127},
+	{Index:'1', Desc:'PREPA', value: 58},
+	{Index:'2', Desc:'COGEN', value: 42},
+	{Index:'3', Desc:'Reserva en Rotación', value: 414},
+	{Index:'4', Desc:'Reserva Operacional', value: 981},
+	{Index:'5', Desc:'Capacidad Disponible', value: 3108},
+	{Index:'6', Desc:'Próxima Hora MW', value: 2121},
+	{Index:'7', Desc:'Máxima para Hoy', value: 2231},
+	{Index:'8', Desc:'Máxima Mensual', value: 2648},
+];
+const dataLoadPerSite = [
+	{Index:'0', Type:'Hidroelectricas', Desc:'Dos Bocas', SiteTotal: 0,
+		units:[
+			{Index:'0', Unit:'Hidro 1', MW: 0, MVar:'0', Cost:0, ParentId: '0'},
+			{Index:'1', Unit:'Hidro 2', MW: 0, MVar:'0', Cost:0, ParentId: '0'},
+			{Index:'2', Unit:'Hidro 3', MW: 0, MVar:'0', Cost:0, ParentId: '0'},
+		]
+	},
+	{Index:'1', Type:'Hidroelectricas', Desc:'Caonillas', SiteTotal: 0,
+		units:[
+			{Index:'0', Unit:'Hidro 1', MW: 0, MVar:'0', Cost:0, ParentId: '1'},
+			{Index:'1', Unit:'Hidro 2', MW: 0, MVar:'0', Cost:0, ParentId: '1'},
+		]
+	},
+	{Index:'2', Type:'Hidroelectricas', Desc:'Garzas', SiteTotal: 0,
+		units:[
+			{Index:'0', Unit:'Hidro 1', MW: 0, MVar:'0', Cost:0, ParentId: '2'},
+			{Index:'1', Unit:'Hidro 2', MW: 0, MVar:'0', Cost:0, ParentId: '2'},
+			{Index:'2', Unit:'Hidro 3', MW: 0, MVar:'0', Cost:0, ParentId: '2'},
+		]
+	},
+	{Index:'3', Type:'Hidroelectricas', Desc:'Rio Blanco', SiteTotal: 0,
+		units:[
+			{Index:'0', Unit:'Hidro 1', MW: 0, MVar:'0', Cost:0, ParentId: '3'},
+			{Index:'1', Unit:'Hidro 2', MW: 0, MVar:'0', Cost:0, ParentId: '3'},
+		]
+	},
+	{Index:'4', Type:'Hidroelectricas', Desc:'Toro Negro', SiteTotal: 0,
+		units:[
+			{Index:'0', Unit:'Hidro 1', MW: 0, MVar:'0', Cost:0, ParentId: '4'},
+			{Index:'1', Unit:'Hidro 2', MW: 0, MVar:'0', Cost:0, ParentId: '4'},
+			{Index:'2', Unit:'Hidro 3', MW: 0, MVar:'0', Cost:0, ParentId: '4'},
+			{Index:'3', Unit:'Hidro 4', MW: 0, MVar:'0', Cost:0, ParentId: '4'},
+			{Index:'4', Unit:'Hidro 5', MW: 0, MVar:'0', Cost:0, ParentId: '4'},
+		]
+	},
+	{Index:'5', Type:'Hidroelectricas', Desc:'Yauco', SiteTotal: 0,
+		units:[
+			{Index:'0', Unit:'Hidro 1', MW: 0, MVar:'0', Cost:0, ParentId: '5'},
+			{Index:'1', Unit:'Hidro 2', MW: 0, MVar:'0', Cost:0, ParentId: '5'},
+			{Index:'2', Unit:'Hidro 3', MW: 0, MVar:'0', Cost:0, ParentId: '5'},
+		]
+	},
+	{Index:'6', Type:'COGEN', Desc:'AES', SiteTotal: 254,
+		units:[
+			{Index:'0', Unit:'Unit 1', MW: 254, MVar:'74', Cost:4.38, ParentId: '6'},
+			{Index:'1', Unit:'Unit 2', MW: 0, MVar:'0', Cost:4.38, ParentId: '6'},
+		]
+	},
+	{Index:'7', Type:'COGEN', Desc:'Ecoelectrica', SiteTotal: 429,
+		units:[
+			{Index:'0', Unit:'Gas 1', MW: 146, MVar:'45', Cost:10.77, ParentId: '7'},
+			{Index:'1', Unit:'Gas 2', MW: 146, MVar:'47', Cost:10.77, ParentId: '7'},
+			{Index:'2', Unit:'STG', MW: 138, MVar:'54', Cost:10.77, ParentId: '7'},
+		]
+	},
+	{Index:'8', Type:'Turbina de Gas', Desc:'Estaciones GT', SiteTotal: 0,
+		units:[
+			{Index:'0', Unit:'Palo Seco', MW: 0, MVar:'0', Cost:0, ParentId: '8'},
+			{Index:'1', Unit:'Vega Baja', MW: 0, MVar:'0', Cost:0, ParentId: '8'},
+			{Index:'2', Unit:'Costa Sur', MW: 0, MVar:'0', Cost:0, ParentId: '8'},
+			{Index:'3', Unit:'Jobos', MW: 0, MVar:'0', Cost:0, ParentId: '8'},
+			{Index:'4', Unit:'Daguao', MW: 0, MVar:'0', Cost:0, ParentId: '8'},
+			{Index:'5', Unit:'Yabucoa', MW: 0, MVar:'0', Cost:0, ParentId: '8'},
+			{Index:'6', Unit:'Aguirre', MW: 0, MVar:'0', Cost:18.25, ParentId: 
