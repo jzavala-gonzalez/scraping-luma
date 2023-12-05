f7004acc851b36ef7857f04a081f9c03a4e35e8d
Automated <actions@users.noreply.github.com>
2023-12-05 11:40:49 +0000

    Update genera/dataSource Tue Dec  5 11:40:49 UTC 2023

--- genera/dataSource.js
+++ genera/dataSource.js
@@ -1,17 +1,1 @@
-const timestamp_saved = '12/05/2023 11:35 AM';
-const dataFechaAcualizado = '12/5/2023' + ' 7:31:24 AM';
-const dataFuelCost = [
-	{place: 'San Juan', value: 98},
-	{place: 'San Juan CC', value: 127},
-	{place: 'Palo Seco', value: 97},
-	{place: 'Aguirre', value: 96},
-	{place: 'Aguirre CC', value: 127},
-	{place: 'Costa Sur', value: 92},
-	{place: 'Cambalache', value: 134},
-	{place: 'Mayaguez', value: 127},
-	{place: 'Costa Sur LNG', value: 9},
-];
-const dataByFuel = [
-	{fuel: 'Bunker', value: 22},
-	{fuel: 'Diesel', value: 0},
-	{fuel: 'LNG', value: 65},
+const timestamp_saved = '12/05/2023 11:40 AM';
